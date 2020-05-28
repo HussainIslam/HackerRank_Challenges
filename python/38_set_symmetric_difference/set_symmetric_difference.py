ad50b351de035815271252bb0758880528dda65b
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-05-28 18:32:07 -0400

    python - 38 set symmetric difference

---
+++ python/38_set_symmetric_difference/set_symmetric_difference.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+#!/usr/bin/python
+if __name__ == "__main__":
+    number_of_students_english = int(input())
+    set_of_students_english = set(list(map(int, input().split())))
+    number_of_students_french = int(input())
+    set_of_students_french = set(list(map(int, input().split())))
+    print(len(set_of_students_english.symmetric_difference(set_of_students_french)))
